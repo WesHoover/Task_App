3b1579dc7165461be60e7b877c058a16338ce6dd
Wes Hoover <work@weshoover.com>
2015-06-30 10:28:07 -0500

    fixed err handling if conditions; missing curlies

--- server.js
+++ server.js
@@ -48,9 +48,9 @@ app.get('/api/todos', function(req, res) {
     Todo.find(function(err, todos) {
 
         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if (err)
+        if (err) {
             res.send(err);
-
+        }
         res.json(todos); // return all todos in JSON format
     });
 });
@@ -63,9 +63,9 @@ app.post('/api/todos', function(req, res) {
         text : req.body.text,
         done : false
     }, function(err, todo) {
-        if (err)
+        if (err) {
             res.send(err);
-
+        }
         // get and return all the todos after you create another
         Todo.find(function(err, todos) {
             if (err)
@@ -81,13 +81,14 @@ app.delete('/api/todos/:todo_id', function(req, res) {
     Todo.remove({
         _id : req.params.todo_id
     }, function(err, todo) {
-        if (err)
+        if (err) {
             res.send(err);
-
+        }
         // get and return all the todos after you create another
         Todo.find(function(err, todos) {
-            if (err)
+            if (err) {
                 res.send(err);
+            }
             res.json(todos);
         });
     });
