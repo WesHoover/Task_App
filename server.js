2f2bd4ca114fba236599787066c6d11b7aefdc1c
Wes Hoover <work@weshoover.com>
2015-06-30 12:06:36 -0500

    added angular code to core.js

--- server.js
+++ server.js
@@ -96,6 +96,10 @@ app.delete('/api/todos/:todo_id', function(req, res) {
     });
 });
 
+// application -------------------------------------------------------------
+app.get('*', function(req, res) {
+   res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 
 // listen (start app with node server.js)
 app.listen(5000);
