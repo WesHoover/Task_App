6abe2f2544bc5f25afba9581b2fb5de937355c73
Wes Hoover <work@weshoover.com>
2015-07-01 00:40:46 -0500

    broke out the styles into own file, tweaked angular bits

--- public/core.js
+++ public/core.js
@@ -1,41 +1,44 @@
-var taskApp = angular.module('taskApp', []);
+(function(){
 
-function mainController($scope, $http) {
-    $scope.formData = {};
+  var taskApp = angular.module('taskApp', []);
 
-    // when landing on the page, get all todos and show them
-    $http.get('/api/todos')
-        .success(function(data) {
-            $scope.todos = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
+  function mainController($scope, $http) {
+      $scope.formData = {};
 
-    // when submitting the add form, send the text to the node API
-    $scope.createTodo = function() {
-        $http.post('/api/todos', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
+      // when landing on the page, get all todos and show them
+      $http.get('/api/todos')
+          .success(function(data) {
+              $scope.todos = data;
+              console.log(data);
+          })
+          .error(function(data) {
+              console.log('Error: ' + data);
+          });
 
-    // delete a todo after checking it
-    $scope.deleteTodo = function(id) {
-        $http.delete('/api/todos/' + id)
-            .success(function(data) {
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
+      // when submitting the add form, send the text to the node API
+      $scope.createTodo = function() {
+          $http.post('/api/todos', $scope.formData)
+              .success(function(data) {
+                  $scope.formData = {}; // clear the form so our user is ready to enter another
+                  $scope.todos = data;
+                  console.log(data);
+              })
+              .error(function(data) {
+                  console.log('Error: ' + data);
+              });
+      };
 
-}
+      // delete a todo after checking it
+      $scope.deleteTodo = function(id) {
+          $http.delete('/api/todos/' + id)
+              .success(function(data) {
+                  $scope.todos = data;
+                  console.log(data);
+              })
+              .error(function(data) {
+                  console.log('Error: ' + data);
+              });
+      };
+
+  }
+})();
--- server.js
+++ server.js
@@ -50,9 +50,9 @@ app.get('/api/todos', function(req, res) {
     Todo.find(function(err, todos) {
 
         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if (err) {
+        if (err)
             res.send(err);
-        }
+
         res.json(todos); // return all todos in JSON format
     });
 });
@@ -65,9 +65,9 @@ app.post('/api/todos', function(req, res) {
         text : req.body.text,
         done : false
     }, function(err, todo) {
-        if (err) {
+        if (err)
             res.send(err);
-        }
+
         // get and return all the todos after you create another
         Todo.find(function(err, todos) {
             if (err)
@@ -83,14 +83,14 @@ app.delete('/api/todos/:todo_id', function(req, res) {
     Todo.remove({
         _id : req.params.todo_id
     }, function(err, todo) {
-        if (err) {
+        if (err)
             res.send(err);
-        }
+
         // get and return all the todos after you create another
         Todo.find(function(err, todos) {
-            if (err) {
+            if (err)
                 res.send(err);
-            }
+
             res.json(todos);
         });
     });
