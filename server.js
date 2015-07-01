346b226e121fd6249c301e1cd16e969244200acb
Wes Hoover <work@weshoover.com>
2015-07-01 10:37:45 -0500

    troubleshoot R10 Boot timeout error, commented out all mongoose stuff

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-require('underscore');
+//var underscore = require('underscore');
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose'); // mongoose for mongodb
+// var mongoose = require('mongoose'); // mongoose for mongodb
 var morgan = require('morgan'); // log requests to the console (express4)
 var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 // database connection vars
-var db_prod = 'mongodb://mongo:mongodb1@ds037622.mongolab.com:37622/heroku_v52rd94l';
-var db_dev = 'mongodb://localhost:27017/Task_app';
+// var db_prod = 'mongodb://mongo:mongodb1@ds037622.mongolab.com:37622/heroku_v52rd94l';
+// var db_dev = 'mongodb://localhost:27017/Task_app';
 
 // db connection
 // if (NODE_ENV === "production") {
@@ -17,7 +17,7 @@ var db_dev = 'mongodb://localhost:27017/Task_app';
 
 // } {
   // Use -- DEV - LOCAL connection to mongodb with mongoose
-mongoose.connect('mongodb://mongo:mongodb1@ds037622.mongolab.com:37622/heroku_v52rd94l');
+// mongoose.connect('mongodb://mongo:mongodb1@ds037622.mongolab.com:37622/heroku_v52rd94l');
 // }
 
 
@@ -36,64 +36,64 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(methodOverride());
 
 // set up Todo model
-var Todo = mongoose.model('Todo', {
-      text : String
-  });
+// var Todo = mongoose.model('Todo', {
+//       text : String
+//   });
 
 // routes
 // todos API
 // get all todos
-app.get('/api/todos', function(req, res) {
+// app.get('/api/todos', function(req, res) {
 
-    // use mongoose to get all todos in the database
-    Todo.find(function(err, todos) {
+//     // use mongoose to get all todos in the database
+//     Todo.find(function(err, todos) {
 
-        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if (err)
-            res.send(err);
+//         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+//         if (err)
+//             res.send(err);
 
-        res.json(todos); // return all todos in JSON format
-    });
-});
+//         res.json(todos); // return all todos in JSON format
+//     });
+// });
 
-// create todo and send back all todos after creation
-app.post('/api/todos', function(req, res) {
+// // create todo and send back all todos after creation
+// app.post('/api/todos', function(req, res) {
 
-    // create a todo, information comes from AJAX request from Angular
-    Todo.create({
-        text : req.body.text,
-        done : false
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
+//     // create a todo, information comes from AJAX request from Angular
+//     Todo.create({
+//         text : req.body.text,
+//         done : false
+//     }, function(err, todo) {
+//         if (err)
+//             res.send(err);
 
-        // get and return all the todos after you create another
-        Todo.find(function(err, todos) {
-            if (err)
-                res.send(err);
-            res.json(todos);
-        });
-    });
+//         // get and return all the todos after you create another
+//         Todo.find(function(err, todos) {
+//             if (err)
+//                 res.send(err);
+//             res.json(todos);
+//         });
+//     });
 
-});
+// });
 
-// delete a todo
-app.delete('/api/todos/:todo_id', function(req, res) {
-    Todo.remove({
-        _id : req.params.todo_id
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
+// // delete a todo
+// app.delete('/api/todos/:todo_id', function(req, res) {
+//     Todo.remove({
+//         _id : req.params.todo_id
+//     }, function(err, todo) {
+//         if (err)
+//             res.send(err);
 
-        // get and return all the todos after you create another
-        Todo.find(function(err, todos) {
-            if (err)
-                res.send(err);
+//         // get and return all the todos after you create another
+//         Todo.find(function(err, todos) {
+//             if (err)
+//                 res.send(err);
 
-            res.json(todos);
-        });
-    });
-});
+//             res.json(todos);
+//         });
+//     });
+// });
 
 // application -------------------------------------------------------------
 app.get('*', function(req, res) {
